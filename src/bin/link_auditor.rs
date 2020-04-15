e6b7164afa562d389cee3bd4d7888ccd9c1e3eb4
Simon Egersand <s.egersand@gmail.com>
2020-04-15 22:25:54 +0200

    Add connection timeout option

--- src/bin/link_auditor.rs
+++ src/bin/link_auditor.rs
@@ -14,6 +14,7 @@ use std::path::Path;
 
 static OPT_FILES: &str = "FILES";
 static OPT_WHITE_LIST: &str = "white-list";
+static OPT_TIMEOUT: &str = "timeout";
 
 #[tokio::main]
 async fn main() {
@@ -32,16 +33,28 @@ async fn main() {
         .takes_value(true)
         .required(false);
 
+    let opt_timeout = Arg::with_name(OPT_TIMEOUT)
+        .help("Connection timeout (default: 30)")
+        .short("t")
+        .long(OPT_TIMEOUT)
+        .value_name("seconds")
+        .takes_value(true)
+        .required(false);
+
     let matches = App::new("link_auditor")
         .version(crate_version!())
         .author(crate_authors!())
         .about(crate_description!())
         .arg(opt_word)
         .arg(opt_white_list)
+        .arg(opt_timeout)
         .get_matches();
 
     let auditor = Auditor {};
-    let mut opts = AuditorOptions { white_list: None };
+    let mut opts = AuditorOptions {
+        white_list: None,
+        timeout: None,
+    };
 
     if let Some(white_list_urls) = matches.value_of(OPT_WHITE_LIST) {
         let white_list: Vec<String> = white_list_urls
@@ -50,6 +63,13 @@ async fn main() {
             .filter(|s| !s.is_empty())
             .collect();
         opts.white_list = Some(white_list);
+    }
+
+    if let Some(str_timeout) = matches.value_of(OPT_TIMEOUT) {
+        let timeout: u64 = str_timeout
+            .parse()
+            .expect(format!("Could not parse {} into an int (u64)", str_timeout).as_str());
+        opts.timeout = Some(timeout);
     }
 
     if let Some(files) = matches.values_of(OPT_FILES) {
